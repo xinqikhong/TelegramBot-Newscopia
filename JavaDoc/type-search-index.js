4f1de43920f47270d722cf1b3928b4d9b3e80d3f
xinqikhong <xinqi0620@gmail.com>
2024-05-01 19:37:39 +0800

    JavaDoc

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"my.uum","l":"App"},{"p":"my.uum","l":"AppTest"},{"p":"my.uum","l":"MyNewsBot"},{"p":"my.uum","l":"NewsApiClient"}];updateSearchResults();
